a0f8bb4b8d02080b303de70bcd8e9989620448f0
Rostyslav Lukan <lkn.rostyslav@outlook.com>
2023-02-02 13:49:06 +0100

    f1

---
+++ src/components/PropsProvider.vue
@@ -0,0 +1,17 @@
+<script>
+  export default{  data() {
+    return {
+        inputValue: 0
+    }
+}
+  }
+</script>
+
+<template>
+    <div>
+    <input v-model="inputValue"/>
+    </div>
+</template>
+
+<style>
+</style>
